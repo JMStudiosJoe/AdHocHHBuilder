32a59de938197e6d18ee37300cf2e5d5dbf241d4
Joseph <jrichardson@jmstudios.net>
2017-09-08 11:16:50 -0700

    was using the url the wrong way now getting data from the elements instead of url at first

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 // your code goes here ...
 var persons = []
 var addPersonButton = document.getElementsByClassName('add')[0]
+var householdList = document.getElementsByClassName('household')[0]
+var form = document.forms[0]
+var inputAge = document.getElementsByName('age')[0]
+var selectRel = document.getElementsByName('rel')[0]
+var smokerCheck = document.getElementsByName('smoker')[0]
+
 addPersonButton.addEventListener('click', function(e) {
+    e.preventDefault()
+    console.log(form)
+    console.log(inputAge.value)
+    console.log(selectRel.value)
+    console.log(smokerCheck.value)
+
     var url = new URL(window.location.href)
-    var age = url.searchParams.get('age')
     var rel = url.searchParams.get('rel')
-    var smoker = url.searchParams.get('smoker')
-    var message = 'age: ' + age + ' rel: ' + rel + ' smoker: ' + smoker 
-    alert(message)
-    var person = {
-        'relationship': 'spouse',
-        'smoker': false,
-        'age': 27
+    var smoker = url.searchParams.get('smoker') == 'on' ? true: false
+    var age = url.searchParams.get('age')
+    if (validateAge(Number(age))) {
+        var person = {
+            'age': age,
+            'rel': rel,
+            'smoker': smoker
+        }
+        addPerson(person)
     }
-    addPerson(person)
+    else {
+        //alert('Invalid age please try again, age must be greater than 0')
+    }
 
 }, false)
+
+function validateAge(age) {
+    return age > 0
+}
+
 function addPerson(person) {
-    console.log(person)
+    persons.push(person)
+}
+
+function displayHousehold() {
+    householdList.innerHTML = ''
+    console.log('display householdlist with persons: ', persons.length)
+    for (var person of persons) {
+        var li = document.createElement('li')
+        var personText = 'age: ' + person['age'] + '\nrelationship: ' + person['rel'] + '\nSmoker: ' + person['smoker']
+        li.appendChild(document.createTextNode(personText))
+        householdList.appendChild(li)
+    }
 }
